0b6960bac43c4224eeffac53068dbddf91357d0c
C. Bryan Daniels <cdaniels@nandor.net>
2018-02-15 17:36:14 -0600

    Added bing and google as image search engines

--- image_download.py
+++ image_download.py
@@ -2,100 +2,116 @@
 
 ###
 # C.Bryan Daniels
-# 2/8/2018
-# Very minor tweaks to:  github.com/atif93/google_image_downloader
+# 2/14/2018
+# Adapted from github.com/atif93/google_image_downloader
 ###
 
-import os, sys, time
-import json, requests, shutil
+# Usage: python image_download.py 'searchtext' 'num_images' [--gui]
+
+import sys, time, json, requests, shutil
+from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 
+args = sys.argv
+searchtext = args[1]
+label = searchtext.replace(" ", "_")
+num_images = int(args[2])
 
-# adding path to geckodriver to the OS environment variable
-os.environ["PATH"] += os.pathsep + os.getcwd()
-download_path = "dataset/"
+############################################################################################
+# Customized by search engine
+def get_urls_bing(driver, searchtext):
+    request = f'https://www.bing.com/images/search?q={searchtext}'
+    driver.get(request)
+    # Find Button: "See More Images"
+    for i in range(10):
+        driver.execute_script(f'window.scrollBy(0, 1000)')
+        time.sleep(.2)
+        try:
+            driver.find_element_by_xpath("//a[@class='btn_seemore']").click()
+            print("Found: See More Images->", i)
+            break
+        except:
+            None
+    # Scroll through end of the document
+    for i in range(40):
+        driver.execute_script("window.scrollBy(0, 1000)")
+        print("Scrolling-> ", i)
+        time.sleep(.2)
+    # Get urls of images
+    def make_url(xpath): return json.loads(xpath.get_attribute('m'))['murl']
+    xpaths = driver.find_elements_by_xpath('//a[@class="iusc"]')
+    return [make_url(xpath) for xpath in xpaths]
 
-# Usage: python image_download.py 'searchtext' 'num_requested'
+def get_urls_google(driver, searchtext):
+    scrolls = 1 + int(num_images / 400)
+    request = f'https://www.google.co.in/search?q={searchtext}&source=lnms&tbm=isch'
+    driver.get(request)
+    # Find Button: "See More Images"
+    for i in range(5):
+        for j in range(10):
+            driver.execute_script("window.scrollBy(0, 10000)")
+            time.sleep(0.2)
+        time.sleep(0.5)
+        try:
+            driver.find_element_by_xpath("//input[@value='Show more results']").click()
+            print("Found: See More Images->",i)
+        except Exception as e:
+            break
+    # Get urls
+    def make_url(xpath): return json.loads(xpath.get_attribute('innerHTML'))['ou']
+    xpaths = driver.find_elements_by_xpath('//div[contains(@class,"rg_meta")]')
+    return [make_url(xpath) for xpath in xpaths]
 
-def main():
-        ### Local:
-        # driver = webdriver.Firefox()
+############################################################################################
+def make_driver():
+    if len(args)== 4 and args[3] == '--gui':
+        driver = webdriver.Firefox()
+    else:
+        options = Options()
+        options.add_argument('-headless')
+        driver = Firefox(executable_path='geckodriver', firefox_options=options)
+    return driver
 
-        ## HEADLESS: set to work with headless Firefox, assumes  geckodriver is in the searchpath or is specified in executable_path, below
-	options = Options()
-	options.add_argument('-headless')
-	driver = Firefox(executable_path='geckodriver', firefox_options=options)
-        ##
+def check_suffix(path):
+    if path.suffix not in [ ".jpg", ".jpeg", ".png", ".gif"]:
+        return path.with_suffix(".jpg")
+    else:
+        return path
 
-	searchtext = sys.argv[1]
-	num_requested = int(sys.argv[2])
-	number_of_scrolls = num_requested / 400 + 1
-	# number_of_scrolls * 400 images will be opened in the browser
+def get_and_save_images(urls, label):
+    headers = {'User-Agent' :  "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"}
+    # Make label_dir
+    label_dir = Path('dataset')  / label
+    if not Path.exists(label_dir / label):
+        label_dir.mkdir(parents=True,exist_ok=True)
+    # Get images and save to label_dir
+    num_downloaded = 0
+    for (i, image) in enumerate(urls, 1):
+        try:
+            req = requests.get(image, stream=True, headers=headers)
+            if req.status_code == 200:
+                with open(check_suffix(label_dir / str(num_downloaded)), "wb") as fname:
+                    req.raw.decode_content = True
+                    shutil.copyfileobj(req.raw, fname)
+                    num_downloaded += 1
+                    print (f'Image {i} : {image}')
+        except Exception as e:
+            print (f'Download failed: {e}')
+        finally:
+            None
+        if num_downloaded >= num_images:
+            break
+    print(f'Downloaded {num_downloaded}/{num_images}')
 
-	if not os.path.exists(download_path + searchtext.replace(" ", "_")):
-		os.makedirs(download_path + searchtext.replace(" ", "_"))
+## __Main Script__    
+driver = make_driver()
+#urls = get_urls_bing(driver,searchtext)
+urls = get_urls_google(driver,searchtext)
+print (f'Found {len(urls)} images')
+get_and_save_images(urls, label)
+driver.quit()
 
-	url = f'https://www.google.co.in/search?q={searchtext}&source=lnms&tbm=isch'
 
-
-	driver.get(url)
-
-	headers = {}
-	headers['User-Agent'] = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
-	extensions = { "jpg", "jpeg", "png", "gif" }
-	img_count = 0
-	downloaded_img_count = 0
-
-	for _ in range(int(number_of_scrolls)):
-		for __ in range(10):
-			# multiple scrolls needed to show all 400 images
-			driver.execute_script("window.scrollBy(0, 1000000)")
-			time.sleep(0.2)
-		# to load next 400 images
-		time.sleep(0.5)
-
-		try:
-			driver.find_element_by_xpath("//input[@value='Show more results']").click()
-
-		except Exception as e:
-			print (f'Less images found: {e}')
-			break
-
-	imges = driver.find_elements_by_xpath('//div[contains(@class,"rg_meta")]')
-	print ("Total images:", len(imges), "\n")
-
-	for img in imges:
-
-		img_count += 1
-		img_url = json.loads(img.get_attribute('innerHTML'))["ou"]
-		img_type = json.loads(img.get_attribute('innerHTML'))["ity"]
-		print (f'Downloading image {img_count} : {img_url}')
-
-		try:
-			if img_type not in extensions: img_type = "jpg"
-
-			r = requests.get(img_url, stream=True, headers=headers)
-			if r.status_code == 200:
-				with open(download_path + searchtext.replace(" ", "_") + "/" + str(downloaded_img_count) + "." + img_type, "wb") as f:
-					r.raw.decode_content = True
-					shutil.copyfileobj(r.raw, f)
-					downloaded_img_count += 1
-
-		except Exception as e:
-			print (f'Download failed: {e}')
-
-		finally:
-			print('')
-
-		if downloaded_img_count >= num_requested:
-			break
-
-	print (f'Total downloaded: {downloaded_img_count}/{img_count}')
-	driver.quit()
-
-if __name__ == "__main__":
-	main()
